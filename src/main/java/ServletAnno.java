0fde87ed8db44047dc864becc59073312248958b
MakushevEvgenii <evmakush@gmail.com>
2025-11-30 20:10:17 +0300

    30.11 дриклеить фильтры, лиснеры, и тд

--- src/main/java/ServletAnno.java
+++ src/main/java/ServletAnno.java
@@ -1,13 +1,18 @@
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.Writer;
 
-public class Sd extends HttpServlet {
+@WebServlet("/sdw")
+public class ServletAnno extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        Writer writer = resp.getWriter();
+        writer.write("########");
     }
 }
